9950b1565315efe15bab90106a28b1da7956a43a
kate2797 <katka.zaprazna@icloud.com>
2020-07-27 22:19:40 +0200

    Revert "Added files"
    This reverts commit e55cf51613b074dbb908a00e206714391cc6c0cc.

--- project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/patients', views.patients, name='patients'),
-    path('api/patients/<int:nhs_number>',
-         views.patients_detail, name="patients_detail"),
-]
